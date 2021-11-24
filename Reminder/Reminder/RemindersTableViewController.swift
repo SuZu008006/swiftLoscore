34069b40773b319158965ff39720ff59e9e8031f
SuZu008006 <suzusuke0806@gmail.com>
2021-11-24 10:14:05 +0900

    Add RemindersTableViewController

---
+++ Reminder/Reminder/RemindersTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  RemindersTableViewController.swift
+//  Reminder
+//
+//  Created by Ryosuke Ito on 2021/11/24.
+//
+
+import UIKit
+
+final class RemindersTableViewController:UITableViewController{
+    
+}
